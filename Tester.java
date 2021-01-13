cb243eaf1d475325395cdda69e95931353dd034b
rifath33 <hrifath20@stuy.edu>
2021-01-13 13:59:20 -0500

    tester confirms results ey sala

--- Tester.java
+++ Tester.java
@@ -1,9 +1,14 @@
 public class Tester{
   public static void main(String[] args){
     
-        for(int i = 0; i < 7; i++){
+      /*   for(int i = 0; i < 7; i++){
             System.out.println(Radix.nth(2435235,i));
-        }
+        } */
+        
+        System.out.println(
+            Radix.length(219392)
+        );
+        
         
     
   }
