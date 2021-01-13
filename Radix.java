0bac5a464607fc157fc3a866bee6487465157fc9
rifath33 <hrifath20@stuy.edu>
2021-01-13 04:33:46 -0500

    just pushing

--- Radix.java
+++ Radix.java
@@ -1,3 +1,15 @@
 public class Radix{
   
+  public static int nth(int n, int col){
+      
+  }
+  
+  public static int length(int n){
+      
+  }
+  
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+      
+  }
+  
 }
