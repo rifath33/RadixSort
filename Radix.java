dbfa8c3f065472169e7c01b448aae541edb8bb1c
rifath33 <hrifath20@stuy.edu>
2021-01-13 13:56:16 -0500

    made a working nth method

--- Radix.java
+++ Radix.java
@@ -1,15 +1,18 @@
 public class Radix{
   
   public static int nth(int n, int col){
-      
+    String number = n + "";
+    String thing = number.charAt(col) + "";
+    return Integer.parseInt(thing);
+    
   }
   
-  public static int length(int n){
+/*   public static int length(int n){
       
   }
   
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
       
-  }
+  } */
   
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,10 @@
 public class Tester{
   public static void main(String[] args){
-
+    
+        for(int i = 0; i < 7; i++){
+            System.out.println(Radix.nth(2435235,i));
+        }
+        
+    
   }
 }
